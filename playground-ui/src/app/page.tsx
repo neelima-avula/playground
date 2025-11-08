e4082a38bf18d16d246a2b028acfa1288782ce28
Neelima Avula <neelima@MacBook-Pro-3.local>
2025-11-08 11:14:42 -0500

    added Copilotkit

--- playground-ui/src/app/page.tsx
+++ playground-ui/src/app/page.tsx
@@ -1,9 +1,18 @@
-
+import { CopilotKit } from "@copilotkit/react-core";
+import "@copilotkit/react-ui/styles.css";
+import { CopilotChat } from "@copilotkit/react-ui";
 
 export default function Home() {
   return (
-    <div>
-     Hello
-    </div>
+    <CopilotKit publicApiKey="ck_pub_08367883247b9211900519383e2eafca">
+      <CopilotChat
+    instructions={"You are assisting the user as best as you can. Answer in the best way possible given the data you have."}
+    labels={{
+      title: "Your Assistant",
+      initial: "Hi! 👋 How can I assist you today?",
+    }}
+  />
+  
+      </CopilotKit>
   );
 }
